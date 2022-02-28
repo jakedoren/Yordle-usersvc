6786d1fd2199b1d7a45f153b54fd099372a24c8c
Jacob Doren <jakedoren@gmail.com>
2022-02-28 16:55:34 -0500

    unique index

--- src/main/java/com/yordle/usersvc/model/User.java
+++ src/main/java/com/yordle/usersvc/model/User.java
@@ -1,12 +1,14 @@
 package com.yordle.usersvc.model;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document("users")
 public class User {
     @Id
     private String id;
+    @Indexed(unique = true)
     private String username;
     private String password;
 
